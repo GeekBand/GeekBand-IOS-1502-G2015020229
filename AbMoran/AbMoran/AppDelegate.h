f4b662819e0eeb70a9426ba6471f2b85946ba58e
Uranus <Uranus@uranuss-imac.local>
2015-11-20 17:32:29 +0800

    Init project

---
+++ AbMoran/AbMoran/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AbMoran
+//
+//  Created by Arbin on 11/20/15.
+//  Copyright © 2015 com.GeekBand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
