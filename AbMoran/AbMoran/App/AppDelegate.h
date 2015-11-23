0282d0b215959e0c2bb827eaa11b04c34b5eb445
Uranus <Uranus@uranuss-imac.corp.carestreamhealth.com>
2015-11-23 14:22:52 +0800

    init

---
+++ AbMoran/AbMoran/App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ABMoran
+//
+//  Created by Arbin on 11/23/15.
+//  Copyright © 2015 com.Geekband. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
