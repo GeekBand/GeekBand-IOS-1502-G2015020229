bc14091e8d6fb723b0ec25c45b23ce936c667f4e
Uranus <Uranus@uranuss-imac.local>
2015-11-23 14:36:20 +0800

    add login

---
+++ AbMoran/AbMoran/Controllers/LoginAndRegister/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  AbMoran
+//
+//  Created by Arbin on 11/23/15.
+//  Copyright © 2015 com.Geekband. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
